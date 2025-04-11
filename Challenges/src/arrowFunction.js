b6aac3ba7f0f43e9b11fcbe2c2ae46883b2946d3
Andres Mauricio <andres123456jkil@gmail.com>
2025-04-10 23:30:58 -0500

    Funciones flecha y normales

---
+++ Challenges/src/arrowFunction.js
@@ -0,0 +1,10 @@
+const checkOddEvenArrow = (num) => {
+    if (num % 2 === 0) {
+      console.log(`${num} is even`);
+    } else {
+      console.log(`${num} is odd`);
+    }
+};
+  
+checkOddEvenArrow(8);
+  
